a697e82990dca3f060179a18f1b8dcc19a544bfe
InvictusKhan <fasihkhan8352@gmail.com>
2026-06-22 03:03:48 +0400

    Day - 1
    Tested somethings from memory wrote a function, tested template literal, variable and used basic arithmetic function.

--- Java.js
+++ Java.js
@@ -10,3 +10,18 @@ function add(x, y){
 }
 add(5, 4);
 
+
+// let name = "Fasih";
+// let last = "Khan"
+// console.log(name,last);
+
+let first = "invictus";
+let last = "Khan";
+
+function age(a, b){
+    trueAge = result = a - b;
+    console.log(`Hello your name is ${first, last} and your age is ${trueAge}`)
+
+}
+
+age(2026, 1994);
