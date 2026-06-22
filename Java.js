2bff270cdd988a118caf3aa55a09580fdbf2c134
InvictusKhan <fasihkhan8352@gmail.com>
2026-06-23 02:47:07 +0400

    New Test

--- Java.js
+++ Java.js
@@ -3,31 +3,35 @@
 // let y = 5;
 
 
-//This is add function
-function add(x, y){
-    console.log(x + y)
+// //This is add function
+// function add(x, y){
+//     console.log(x + y)
 
-}
-add(5, 4);
+// }
+// add(5, 4);
 
 
 // let name = "Fasih";
 // let last = "Khan"
 // console.log(name,last);
 
-let first = "invictus";
-let last = "Khan";
+// let first = "invictus";
+// let last = "Khan";
 
-function age(a, b){
-    trueAge = result = a - b;
-    console.log(`Hello your name is ${first, last} and your age is ${trueAge}`)
+// function age(a, b){
+//     trueAge = result = a - b;
+//     console.log(`Hello your name is ${first, last} and your age is ${trueAge}`)
 
-}
+// }
 
-age(2026, 1994);
-// adding this comment just to test if github is working
-let x = 5;
-let y = 10;
-let addition = x + y;
+// age(2026, 1994);
+// // adding this comment just to test if github is working
+// let x = 5;
+// let y = 10;
+// let addition = x + y;
 
-console.log (addition);
+// console.log (addition);
+
+let x;
+x = 5;
+console.log(x);
